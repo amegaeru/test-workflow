bb8db00133e733f0c4a892651efc224ec8df31c2
高橋 貴洋 <ta-takahashi@iret.co.jp>
2026-03-28 17:38:41 +0900

    test-20260328

--- terraform/github-actions-oidc/main.tf
+++ terraform/github-actions-oidc/main.tf
@@ -1,23 +1,18 @@
-# resource "aws_iam_openid_connect_provider" "github_actions" {
-#   url             = "https://token.actions.githubusercontent.com"
-#   client_id_list  = ["sts.amazonaws.com"]
-#   thumbprint_list = var.github_oidc_thumbprint_list
-#
-#   tags = var.tags
-# }
-
-# resource "tls_private_key" "transfer_family" {
-#   algorithm = "RSA"
-#   rsa_bits  = 4096
-# }
+locals {
+  environments = var.environments
+}
 
 resource "aws_s3_bucket" "site_bucket" {
-  bucket = "${var.resource_prefix}${var.s3_bucket_name}"
-  tags   = var.tags
+  for_each = local.environments
+
+  bucket = "${var.resource_prefix}${each.value.s3_bucket_name}"
+  tags   = merge(var.tags, { Environment = each.key })
 }
 
 resource "aws_s3_bucket_public_access_block" "site_bucket" {
-  bucket = aws_s3_bucket.site_bucket.id
+  for_each = local.environments
+
+  bucket = aws_s3_bucket.site_bucket[each.key].id
 
   block_public_acls       = true
   block_public_policy     = true
@@ -26,30 +21,34 @@ resource "aws_s3_bucket_public_access_block" "site_bucket" {
 }
 
 resource "aws_cloudfront_origin_access_control" "site_bucket" {
-  name                              = "${var.resource_prefix}${var.s3_bucket_name}-oac"
-  description                       = "OAC for ${var.resource_prefix}${var.s3_bucket_name}"
+  for_each = local.environments
+
+  name                              = "${var.resource_prefix}${each.value.s3_bucket_name}-oac"
+  description                       = "OAC for ${var.resource_prefix}${each.value.s3_bucket_name}"
   origin_access_control_origin_type = "s3"
   signing_behavior                  = "always"
   signing_protocol                  = "sigv4"
 }
 
 resource "aws_cloudfront_distribution" "site" {
+  for_each = local.environments
+
   enabled             = true
   is_ipv6_enabled     = true
-  comment             = var.cloudfront_comment
+  comment             = each.value.cloudfront_comment
   default_root_object = "index.html"
-  price_class         = var.cloudfront_price_class
+  price_class         = each.value.cloudfront_price_class
 
   origin {
-    domain_name              = aws_s3_bucket.site_bucket.bucket_regional_domain_name
-    origin_id                = aws_s3_bucket.site_bucket.id
-    origin_access_control_id = aws_cloudfront_origin_access_control.site_bucket.id
+    domain_name              = aws_s3_bucket.site_bucket[each.key].bucket_regional_domain_name
+    origin_id                = aws_s3_bucket.site_bucket[each.key].id
+    origin_access_control_id = aws_cloudfront_origin_access_control.site_bucket[each.key].id
   }
 
   default_cache_behavior {
     allowed_methods        = ["GET", "HEAD", "OPTIONS"]
     cached_methods         = ["GET", "HEAD"]
-    target_origin_id       = aws_s3_bucket.site_bucket.id
+    target_origin_id       = aws_s3_bucket.site_bucket[each.key].id
     viewer_protocol_policy = "redirect-to-https"
     compress               = true
 
@@ -74,10 +73,12 @@ resource "aws_cloudfront_distribution" "site" {
 
   depends_on = [aws_s3_bucket_public_access_block.site_bucket]
 
-  tags = var.tags
+  tags = merge(var.tags, { Environment = each.key })
 }
 
 data "aws_iam_policy_document" "site_bucket_policy" {
+  for_each = local.environments
+
   statement {
     sid    = "AllowCloudFrontRead"
     effect = "Allow"
@@ -90,23 +91,27 @@ data "aws_iam_policy_document" "site_bucket_policy" {
     actions = ["s3:GetObject"]
 
     resources = [
-      "${aws_s3_bucket.site_bucket.arn}/*"
+      "${aws_s3_bucket.site_bucket[each.key].arn}/*"
     ]
 
     condition {
       test     = "StringEquals"
       variable = "AWS:SourceArn"
-      values   = [aws_cloudfront_distribution.site.arn]
+      values   = [aws_cloudfront_distribution.site[each.key].arn]
     }
   }
 }
 
 resource "aws_s3_bucket_policy" "site_bucket" {
-  bucket = aws_s3_bucket.site_bucket.id
-  policy = data.aws_iam_policy_document.site_bucket_policy.json
+  for_each = local.environments
+
+  bucket = aws_s3_bucket.site_bucket[each.key].id
+  policy = data.aws_iam_policy_document.site_bucket_policy[each.key].json
 }
 
 data "aws_iam_policy_document" "github_actions_trust" {
+  for_each = local.environments
+
   statement {
     sid     = "GitHubActionsAssumeRole"
     effect  = "Allow"
@@ -127,7 +132,7 @@ data "aws_iam_policy_document" "github_actions_trust" {
       test     = "StringLike"
       variable = "token.actions.githubusercontent.com:sub"
       values = [
-        for branch in var.github_branches :
+        for branch in each.value.github_branches :
         "repo:${var.github_repository}:ref:refs/heads/${branch}"
       ]
     }
@@ -135,12 +140,16 @@ data "aws_iam_policy_document" "github_actions_trust" {
 }
 
 resource "aws_iam_role" "github_actions" {
-  name               = "${var.resource_prefix}${var.role_name}"
-  assume_role_policy = data.aws_iam_policy_document.github_actions_trust.json
-  tags               = var.tags
+  for_each = local.environments
+
+  name               = "${var.resource_prefix}${each.value.role_name}"
+  assume_role_policy = data.aws_iam_policy_document.github_actions_trust[each.key].json
+  tags               = merge(var.tags, { Environment = each.key })
 }
 
 data "aws_iam_policy_document" "github_actions_s3" {
+  for_each = local.environments
+
   statement {
     sid    = "AllowBucketMetadata"
     effect = "Allow"
@@ -149,7 +158,7 @@ data "aws_iam_policy_document" "github_actions_s3" {
       "s3:ListBucket"
     ]
     resources = [
-      aws_s3_bucket.site_bucket.arn
+      aws_s3_bucket.site_bucket[each.key].arn
     ]
   }
 
@@ -162,18 +171,23 @@ data "aws_iam_policy_document" "github_actions_s3" {
       "s3:DeleteObject"
     ]
     resources = [
-      "${aws_s3_bucket.site_bucket.arn}/js/*"
+      "${aws_s3_bucket.site_bucket[each.key].arn}/js/*",
+      "${aws_s3_bucket.site_bucket[each.key].arn}/backup/js-previous/*"
     ]
   }
 }
 
 resource "aws_iam_role_policy" "github_actions_s3" {
-  name   = "${var.resource_prefix}${var.role_name}-s3"
-  role   = aws_iam_role.github_actions.id
-  policy = data.aws_iam_policy_document.github_actions_s3.json
+  for_each = local.environments
+
+  name   = "${var.resource_prefix}${each.value.role_name}-s3"
+  role   = aws_iam_role.github_actions[each.key].id
+  policy = data.aws_iam_policy_document.github_actions_s3[each.key].json
 }
 
 data "aws_iam_policy_document" "github_actions_cloudfront" {
+  for_each = local.environments
+
   statement {
     sid    = "AllowCreateInvalidation"
     effect = "Allow"
@@ -181,87 +195,15 @@ data "aws_iam_policy_document" "github_actions_cloudfront" {
       "cloudfront:CreateInvalidation"
     ]
     resources = [
-      aws_cloudfront_distribution.site.arn
+      aws_cloudfront_distribution.site[each.key].arn
     ]
   }
 }
 
 resource "aws_iam_role_policy" "github_actions_cloudfront" {
-  name   = "${var.resource_prefix}${var.role_name}-cloudfront"
-  role   = aws_iam_role.github_actions.id
-  policy = data.aws_iam_policy_document.github_actions_cloudfront.json
+  for_each = local.environments
+
+  name   = "${var.resource_prefix}${each.value.role_name}-cloudfront"
+  role   = aws_iam_role.github_actions[each.key].id
+  policy = data.aws_iam_policy_document.github_actions_cloudfront[each.key].json
 }
-
-# data "aws_iam_policy_document" "transfer_family_trust" {
-#   statement {
-#     effect  = "Allow"
-#     actions = ["sts:AssumeRole"]
-#
-#     principals {
-#       type        = "Service"
-#       identifiers = ["transfer.amazonaws.com"]
-#     }
-#   }
-# }
-#
-# resource "aws_iam_role" "transfer_family_access" {
-#   name               = "${var.resource_prefix}transfer-family-access"
-#   assume_role_policy = data.aws_iam_policy_document.transfer_family_trust.json
-#   tags               = var.tags
-# }
-#
-# data "aws_iam_policy_document" "transfer_family_access" {
-#   statement {
-#     sid    = "ListBucket"
-#     effect = "Allow"
-#     actions = [
-#       "s3:GetBucketLocation",
-#       "s3:ListBucket"
-#     ]
-#     resources = [aws_s3_bucket.site_bucket.arn]
-#   }
-#
-#   statement {
-#     sid    = "ManageHtmlObjects"
-#     effect = "Allow"
-#     actions = [
-#       "s3:DeleteObject",
-#       "s3:GetObject",
-#       "s3:PutObject"
-#     ]
-#     resources = [
-#       "${aws_s3_bucket.site_bucket.arn}/html/*"
-#     ]
-#   }
-# }
-#
-# resource "aws_iam_role_policy" "transfer_family_access" {
-#   name   = "${var.resource_prefix}transfer-family-access"
-#   role   = aws_iam_role.transfer_family_access.id
-#   policy = data.aws_iam_policy_document.transfer_family_access.json
-# }
-#
-# resource "aws_transfer_server" "site_upload" {
-#   endpoint_type          = "PUBLIC"
-#   identity_provider_type = "SERVICE_MANAGED"
-#   protocols              = ["SFTP"]
-#   security_policy_name   = "TransferSecurityPolicy-2023-05"
-#
-#   tags = var.tags
-# }
-#
-# resource "aws_transfer_user" "site_upload" {
-#   server_id      = aws_transfer_server.site_upload.id
-#   user_name      = var.transfer_user_name
-#   role           = aws_iam_role.transfer_family_access.arn
-#   home_directory = "/${aws_s3_bucket.site_bucket.bucket}/html"
-#
-#   tags = var.tags
-# }
-#
-# resource "aws_transfer_ssh_key" "site_upload" {
-#   server_id = aws_transfer_server.site_upload.id
-#   user_name = aws_transfer_user.site_upload.user_name
-#   body      = tls_private_key.transfer_family.public_key_openssh
-# }
-
